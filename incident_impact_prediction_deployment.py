43e930fae7b3aab1f34e6b34e330204e384c0dcc
Omkar Katkar <89191550+omkarkatkar525@users.noreply.github.com>
2022-03-07 11:31:31 +0530

    Update incident_impact_prediction_deployment.py

--- incident_impact_prediction_deployment.py
+++ incident_impact_prediction_deployment.py
@@ -47,9 +47,10 @@ def main():
     urgency = st.sidebar.selectbox("Select Urgency",["0", "1","2"])
     knowledge = st.sidebar.selectbox("Select Knowledge",["0", "1"])
     u_priority_confirmation = st.sidebar.selectbox("Select u_priority_confirmation",["0", "1"])
+    index = st.text_input("index","Type Here")
     column_1, column_2 = st.beta_columns(2)
-    column_1.markdown(index = st.text_input("index","Type Here"),unsafe_allow_html=True)
-    #index = st.text_input("index","Type Here")
+    column_1.markdown(index,unsafe_allow_html=True)
+    
     number = st.text_input("number","Type Here")
     opened_by = st.text_input("opened_by","Type Here")
     resolved_by = st.text_input("resolved_by","Type Here")
